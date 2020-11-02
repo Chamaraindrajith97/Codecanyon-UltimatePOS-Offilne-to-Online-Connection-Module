0596f7ad26aec9eb6c21890d35075117080ada84
Chamara Indrajith <51538345+Chamaraindrajith97@users.noreply.github.com>
2020-11-02 15:45:31 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+// 1st server - Step 2 - fetching_data.php | Export SQL
+$mysqlDatabaseName ='';
+$mysqlUserName ='root';
+$mysqlPassword ='';
+$mysqlHostName ='127.0.0.1';
+$mysqlExportPath ='offlineerp.sql';
+
+// 2nd server - Step 1 - connecting-to-the-server.php | Delete 2nd server data
+// 2nd server - Step 3 - uploading.php | Import SQL
+$host = "";
+$uname = "";
+$pass = "";
+$database = ""; //Change Your Database Name
+$filename = 'offlineerp.sql';
+
+?>
